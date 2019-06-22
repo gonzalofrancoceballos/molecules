236d4b622ad793103802befdf417642a01a27de7
Gonzalo Franco Ceballos <gonzalofrancoceballos@MBP-de-Gonzalo.home>
2019-06-22 11:22:28 +0200

    adding preprocessing module

---
+++ preprocessing.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+
+
+def read_and_process_structured(structures_path):
+    """
+    Reads structures files and parse them into a single table
+    
+    :param structures_path: path where all files are located (type: tr)
+    :return: table with all molecule structures (type: pd.DataFrame)
+    """
+    
+    xyz_files = os.listdir(structures_path)
+    xyz_files = list(filter(lambda x : '.xyz' in x , xyz_files))
+
+    processed_atoms = []
+    for file in xyz_files:
+        raw_xyz = read_file(structure_path)
+        n_atoms, atoms = process_xyz(raw_xyz)
+
+        processed_atoms.append(atoms)
+
+    processed_atoms = np.vstack(processed_atoms)
+    processed_atoms = pd.DataFrame(processed_atoms)
+    processed_atoms.columns = ['molecule_name','element', 'x', 'y', 'z', 'atom_index']
+    
+    return processed_atoms
+
+
+
+def process_xyz(raw_xyz):
+    """
+    Process a raw molecule file in ".xyz" format into a np.array
+    
+    :param raw_xyz: raw file (type: str)
+    :return: processed molecule (type: np.array)
+    """
+    
+    xyz_lines = raw_xyz.split("\n")
+
+    n_atoms = int(data_lines[0])
+    molecule_name = file.split(".")[0]
+    molecule_name = np.repeat(molecule_name, n_atoms).reshape([-1,1])
+
+    atoms = []
+    for atom in xyz_lines[2:]:
+        atoms.append(atom.split(" "))
+    
+    atom_index = np.arange(n_atoms).reshape([-1,1])
+    atoms = np.hstack([molecule_name, np.array(atoms), atom_index])
+
+    return n_atoms, atoms
+
+
+def read_file(file_path, encoding="utf-8"):
+    """
+    Read a file into a string
+    
+    :param file_path: path to file (type: str)
+    :param encoding: encoding of file (type: str)
+    :return: content of file (type: str)
+    """
+    
+    with open(file_path, encoding=encoding) as f:
+        raw_xyz = f.read()
+    return raw_xyz
